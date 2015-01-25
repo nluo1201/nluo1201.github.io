c3ee3538876cbf83404646e53dacd690e9ee2075
Henry Luong <luong056@umn.edu>
2015-01-25 01:01:38 -0600

    committing changes

--- Robotics/getimages.php
+++ Robotics/getimages.php
@@ -5,7 +5,7 @@ Header("content-type: application/x-javascript");
 //This function gets the file names of all images in the current directory
 //and ouputs them as a JavaScript array
 function returnimages($dirname=".") {
-$pattern="(\.JPG$)|(\.PNG$)|(\.JPEG$)|(\.GIF$)"; //valid image extensions
+$pattern="(\.jpg$)|(\.png$)|(\.jpge$)|(\.gif$)"; //valid image extensions
 $files = array();
 $curimage=0;
 if($handle = opendir($dirname)) {
